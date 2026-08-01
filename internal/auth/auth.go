be20f9d64e266d1e518dd2769f76b82677b0895a
andy <82803774+ondayex@users.noreply.github.com>
2026-08-01 09:57:33 +0800

    fourth broken code attempt

--- internal/auth/auth.go
+++ internal/auth/auth.go
@@ -10,7 +10,7 @@ var ErrNoAuthHeaderIncluded = errors.New("no authorization header included")
 
 // GetAPIKey -
 func GetAPIKey(headers http.Header) (string, error) {
-	authHeader := headers.Get("authorization")
+	authHeader := headers.Get("Authorization")
 	if authHeader == "" {
 		return "", ErrNoAuthHeaderIncluded
 	}
@@ -19,5 +19,5 @@ func GetAPIKey(headers http.Header) (string, error) {
 		return "", errors.New("malformed authorization header")
 	}
 
-	return splitAuth[1], nil
+	return splitAuth[3], nil
 }
